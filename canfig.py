a605ca7e25e2c75533c4ac9ec3b69ca132ac32e8
a42015330-debug <a42015330@gmail.com>
2026-07-16 14:29:41 +0330

    Create canfig.py

---
+++ canfig.py
@@ -0,0 +1,39 @@
+# config.py
+
+BOT_TOKEN = "BOT_TOKEN"
+
+
+# واحد پول بازی
+MONEY_NAME = "تومان"
+
+
+# شروع بازیکن
+
+START_MONEY = 10000
+
+START_POPULATION = 100000
+
+
+# نیروهای اولیه
+
+START_SOLDIERS = 100
+
+START_TANKS = 2
+
+START_PLANES = 2
+
+START_SHIPS = 2
+
+START_MISSILES = 0
+
+START_DEFENSE = 50
+
+
+# دیتابیس
+
+DATABASE_NAME = "world_war.db"
+
+
+# نسخه بازی
+
+GAME_VERSION = "1.0"
