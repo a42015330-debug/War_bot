f49c27955808853f565efe8ce8870f0b114b0cb6
a42015330-debug <a42015330@gmail.com>
2026-07-18 10:52:20 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,30 @@
+# config.py
+
+# توکن ربات
+BOT_TOKEN = "BOT_TOKEN"
+
+# نام مدیر
+ADMIN_USERNAME = "Amirrehi1"
+
+# کانال رسمی
+CHANNEL_LINK = "https://t.me/atlarsanibot"
+
+# واحد پول
+MONEY_NAME = "تومان"
+
+# اطلاعات اولیه بازی
+START_MONEY = 1000
+START_POPULATION = 100000
+
+START_SOLDIERS = 100
+START_TANKS = 0
+START_PLANES = 0
+START_SHIPS = 0
+START_MISSILES = 0
+START_DEFENSE = 50
+
+# دیتابیس
+DATABASE_NAME = "world_war.db"
+
+# نسخه بازی
+GAME_VERSION = "1.0"
